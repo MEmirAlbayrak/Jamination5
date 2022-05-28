54431dd70746658a3ee002260214f5e72be34f67
Mert YILMAZ <mertyilmazgd@gmail.com>
2022-05-29 00:25:13 +0300

    PickUps Prototype

---
+++ Assets/Scripts/PickUps/DoubleSpeed.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class DoubleSpeed : MonoBehaviour
+{
+    
+    [SerializeField] private BoxCollider2D bxcollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "") //Player name
+        {
+            //play sound
+            //other.gameObject.GetComponent<GameObject>() //Double the speed
+            Destroy(this);
+        }
+    }
+}
